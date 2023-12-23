30424eff1359bbc1a31c841b76d6f61cc206e5cc
Nkot117 <kumakura117@gmail.com>
2023-12-23 16:41:01 +0900

    データベースクラスを作成

---
+++ app/src/main/java/com/nkot/todolist/database/AppDatabase.kt
@@ -0,0 +1,31 @@
+package com.nkot.todolist.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.nkot.todolist.database.Task.TaskDao
+import com.nkot.todolist.database.Task.TaskEntity
+
+@Database(entities = [TaskEntity::class], version = 1)
+@TypeConverters(Converters::class)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+
+     companion object {
+        private var INSTANCE: AppDatabase? = null
+
+         fun getInstance(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
