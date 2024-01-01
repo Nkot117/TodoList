65859006311b41f8324d291b3b30c7d220c3002b
Nkot117 <kumakura117@gmail.com>
2024-01-01 15:35:20 +0900

    DatePickerを使用して期限を設定できるように変更

--- app/src/main/java/com/nkot/todolist/ui/TaskAdd/TaskAddFragment.kt
+++ app/src/main/java/com/nkot/todolist/ui/TaskAdd/TaskAddFragment.kt
@@ -6,14 +6,20 @@ import android.os.Bundle
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.nkot.todolist.databinding.FragmentTaskAddBinding
+import com.nkot.todolist.ui.dialog.DatePickerFragment
 
 class TaskAddFragment : BottomSheetDialogFragment() {
     private var _binding: FragmentTaskAddBinding? = null
     private val binding get() = _binding!!
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         _binding = FragmentTaskAddBinding.inflate(layoutInflater)
         val dialog = BottomSheetDialog(requireContext())
         dialog.setContentView(binding.root)
+        binding.editTaskDeadline.setOnClickListener{
+            val dialog = DatePickerFragment()
+            dialog.show(this.childFragmentManager, DatePickerFragment.TAG)
+        }
         return dialog
     }
 
---
+++ app/src/main/java/com/nkot/todolist/ui/dialog/DatePickerFragment.kt
@@ -0,0 +1,50 @@
+package com.nkot.todolist.ui.dialog
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.widget.DatePicker
+import android.widget.EditText
+import androidx.fragment.app.DialogFragment
+import com.nkot.todolist.R
+import com.nkot.todolist.ui.TaskAdd.TaskAddFragment
+import java.util.Calendar
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DatePickerFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        return DatePickerDialog(
+            requireContext(),
+            this,
+            year,
+            month,
+            day
+        )
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        val fragment = parentFragment as TaskAddFragment
+        val editDeadlineTextView = fragment.dialog?.findViewById<EditText>(R.id.edit_task_deadline)
+        val deadlineText = getString(R.string.deadline_text_format, year, month + 1, dayOfMonth)
+        editDeadlineTextView?.setText(deadlineText)
+    }
+
+    companion object {
+        const val TAG = "DatePickerFragment"
+        fun newInstance(edit: EditText): DatePickerFragment {
+            val dialog = DatePickerFragment()
+            return dialog
+        }
+    }
+
+}
