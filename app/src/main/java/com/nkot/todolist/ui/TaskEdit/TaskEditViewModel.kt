218182450a92dd2b9c3375f55e61686ad40671a8
Nkot117 <kumakura117@gmail.com>
2024-02-04 20:19:15 +0900

    不要なimportを削除

--- app/src/main/java/com/nkot/todolist/ui/TaskEdit/TaskEditViewModel.kt
+++ app/src/main/java/com/nkot/todolist/ui/TaskEdit/TaskEditViewModel.kt
@@ -1,11 +1,11 @@
 package com.nkot.todolist.ui.TaskEdit
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.nkot.todolist.database.Task.TaskDao
 import com.nkot.todolist.database.Task.TaskEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
+
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
--- app/src/main/java/com/nkot/todolist/ui/TaskList/TaskListViewModel.kt
+++ app/src/main/java/com/nkot/todolist/ui/TaskList/TaskListViewModel.kt
@@ -1,7 +1,6 @@
 package com.nkot.todolist.ui.TaskList
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.nkot.todolist.database.Task.TaskDao
 import com.nkot.todolist.database.Task.TaskEntity
